04f243ab12dcd7b8a6f789d18c1f27739eb8791a
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-06-27 22:11:59 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const ref = useRef(null);
+
+  useEffect(() => console.log(ref));
+
+  const handleFocus = () => {
+    console.log(ref.current.focus());
+    ref.current.style.color = "red";
+  };
+
+  return (
+    <div className="App">
+      <div className="form">
+        <input ref={ref} />
+        <button onClick={handleFocus}>focus</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
